8c9312d58b7d8d4b2edab0b699976181603a21f0
samurenko roma <samurenkoroma@gmail.com>
2022-03-26 14:21:13 +0200

    added config

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"tAoD-advance/internal/config"
 	"tAoD-advance/internal/user"
 	"tAoD-advance/pkg/logging"
 	"time"
@@ -13,21 +18,40 @@ func main() {
 	logger := logging.GetLogger()
 	logger.Tracef("create router")
 	router := httprouter.New()
+
+	cfg := config.GetConfig()
 	user.NewHandler(logger).Register(router)
-	start(router)
+	start(router, cfg)
 
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
-	listener, err := net.Listen("tcp", "127.0.0.1:1234")
-	if err != nil {
-		panic(err)
+	var listener net.Listener
+	var listenError error
+	if cfg.Listen.Type == "sock" {
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+		logger.Debugf("socket path: %s", socketPath)
+		logger.Info("listen unix socket")
+		listener, listenError = net.Listen("unix", socketPath)
+		logger.Printf("run server on %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenError = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port))
+		logger.Printf("run server %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
+
+	}
+	if listenError != nil {
+		logger.Fatal(listenError)
 	}
 	server := &http.Server{
 		Handler:      router,
 		WriteTimeout: 15 * time.Second,
 	}
-	logger.Printf("run server")
 	logger.Fatal(server.Serve(listener))
 }
--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -34,12 +34,12 @@ type Logger struct {
 	*logrus.Entry
 }
 
-func GetLogger() Logger {
-	return Logger{e}
+func GetLogger() *Logger {
+	return &Logger{e}
 }
 
-func (l *Logger) getLoggerWithField(k string, v interface{}) Logger {
-	return Logger{l.WithField(k, v)}
+func (l *Logger) getLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
 }
 
 func init() {
